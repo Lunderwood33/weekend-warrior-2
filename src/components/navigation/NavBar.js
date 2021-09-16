48b11e468330729c97343c861573927519974409
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-09-16 10:58:29 -0500

    completed the register user error and finished the nav bar links and routes

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -4,13 +4,53 @@ import "./NavBar.css"
 
 export const NavBar = (props) => {
     return (
+
+        <>
         <ul className="navbar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/">Home</Link>
+        <li className="navbar__item active">
+                <Link className="navbar__link" to exact="/"
+                >
+                    Home
+                </Link>
+                </li>
+                
+        <li className="navbar__item active">
+                <Link className="navbar__link" to="/maintenance"
+                >
+                    Maintenance
+                </Link>
+                </li>
+                <li className="navbar__item active">
+                <Link className="navbar__link" to="/parts"
+                >
+                    Parts
+                </Link>
             </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/login">Log out</Link>
+        <li className="navbar__item active">
+                <Link className="navbar__link" to="#"
+                onClick={
+                    () => {
+                        localStorage.removeItem("weekendWarrior_user_id")
+                    }
+                }>
+                    Logout
+                </Link>
             </li>
-          </ul>
+        </ul>
+    </>
+        // <ul className="navbar">
+        //     <li className="navbar__item active">
+        //         <Link className="navbar__link" to= "/">Home</Link>
+        //     </li>
+        //     <li className="navbar__item">
+        //         <Link className="logoutButton" className="navbar__link" to="/vehicles">Vehicles</Link>
+        //     </li>
+        //     <li className="navbar__item">
+        //         <Link className="logoutButton" className="navbar__link" to="/parts">Parts</Link>
+        //     </li>
+        //     <li className="navbar__item">
+        //         <Link className="logoutButton" className="navbar__link" to="/login">Log out</Link>
+        //     </li>
+        //   </ul>
     )
 }
